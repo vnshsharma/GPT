83237da16aa316e54b6a4f0ef055c7b07a2fc1a6
Vansh Sharma <xplore.vansh@gmail.com>
2026-07-15 17:28:37 +0530

    Add the Bigram Language Model

--- Nano Model/model.py
+++ Nano Model/model.py
@@ -74,3 +74,42 @@ for b in range(batch_size):
         context = xb[b, :t+1]
         target = yb[b,t]
         print(f'when input is {context.tolist()} the target: {target}')
+
+
+# implement the bigram language model in this model to train the overall sample data and the validation data 
+# It is not the powerful enough to understand long context
+# But it introduces the core concepts of language modeling
+# training, loss calculaton, and the text generation
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+torch.manual_seed(1337)
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self,vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size,vocab_size)
+    def forward(self, idx, targets = None):
+        logits = self.token_embedding_table(idx)
+        if targets is None:
+            loss = None
+        else: 
+            B,T,C = logits.shape 
+            logits = logits.view(B*T,C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss 
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:,-1,:]
+            probs = F.softmax(logits,dim=-1)
+            idx_next = torch.multinomial(probs,num_samples=1)
+            idx = torch.cat((idx,idx_next),dim=1)
+        return idx
+m = BigramLanguageModel(vocab_size)
+logits, loss = m(xb,yb)
+print(logits.shape)
+print(loss)
+print(decode(m.generate(idx=torch.zeros((1,1),dtype=torch.long),max_new_tokens=100)[0].tolist()))
