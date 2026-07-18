1da23e85a503b29fe6c030d08a32a7db4bb1fa38
Vansh Sharma <xplore.vansh@gmail.com>
2026-07-18 12:28:30 +0530

    add __mul__ for update arthematics

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 def f(x):
     return 3*x**2 - 4*x + 5
-print(f(3.0))
+# print(f(3.0))
 
 xs = np.arange(-5,5,0.25)
 ys = f(xs)
@@ -39,12 +39,20 @@ d2 = a*b + c
 class Value:
     def __init__(self,data):
         self.data = data
+
     def __repr__(self):
         return f"Value(data={self.data})"
+    
     def __add__(self,other):
         out = Value(self.data+ other.data)
         return out
     
+    def __mul__(self,other):
+        out = Value(self.data * other.data)
+        return out
+
 a = Value(2.0)
 b = Value(-3.0)
-print(a+b)
+c = Value(10.0)
+d = a*b+c
+# print(d)
